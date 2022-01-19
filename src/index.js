e86f70ed3204244be4274ef06119a6342cbc774f
Harun Doğdu <harundogdu06@gmail.com>
2022-01-19 22:16:06 +0300

    react-redux added to project

--- src/index.js
+++ src/index.js
@@ -1,5 +1,13 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import App from "./App";
-import 'antd/dist/antd.css';
-ReactDOM.render(<App />, document.getElementById("root"));
+import "antd/dist/antd.css";
+import store from "features/store";
+import { Provider } from "react-redux";
+
+ReactDOM.render(
+  <Provider store={store}>
+    <App />
+  </Provider>,
+  document.getElementById("root")
+);
